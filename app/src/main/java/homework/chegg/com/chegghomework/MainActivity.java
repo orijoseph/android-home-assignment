5b8d26af3e49d3562e0eca3df7053d6d1af67936
Eviatar Saidoff <esaidoff@chegg.com>
2016-08-16 11:59:14 +0300

    Initial commit - added all files

---
+++ app/src/main/java/homework/chegg/com/chegghomework/MainActivity.java
@@ -0,0 +1,13 @@
+package homework.chegg.com.chegghomework;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
