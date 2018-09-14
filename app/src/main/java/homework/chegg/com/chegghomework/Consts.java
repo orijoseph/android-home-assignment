a96dd50d884de78271cfc01725474fe3c0e2ddad
Roni Poyas <rpoyas@chegg.com>
2018-09-14 11:44:26 +0300

    fix source name typo

--- app/src/main/java/homework/chegg/com/chegghomework/Consts.java
+++ app/src/main/java/homework/chegg/com/chegghomework/Consts.java
@@ -1,13 +1,10 @@
 package homework.chegg.com.chegghomework;
 
-/**
- * Created by esaidoff on 16/08/2016.
- */
 public class Consts {
 
     private final static String BASE_URL = "http://chegg-mobile-promotioms.s3.amazonaws.com/android/homework/";
 
-    public final static String DATA_SOURCE_A_URL = BASE_URL + "android_homeword_datasourceA.json";
+    public final static String DATA_SOURCE_A_URL = BASE_URL + "android_homework_datasourceA.json";
     public final static String DATA_SOURCE_B_URL = BASE_URL + "android_homework_datasourceB.json";
     public final static String DATA_SOURCE_C_URL = BASE_URL + "android_homework_datasourceC.json";
 
